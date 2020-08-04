38aad560b16ebeedeed8afe2a1ee557dab18d8ab
FelipeDecome <felipe.decome@hotmail.com>
2020-08-04 19:07:49 -0300

    aula 02 - done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
-
+import cors from "cors";
 import routes from "./routes";
 
 const PORT = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
