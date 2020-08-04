2b261833aabcf0d2df329ea8bafc5ae09e79b6b2
Felipe Decome <felipe.decome@hotmail.com>
2020-08-04 16:59:47 -0300

    aula 02 - almost done

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,79 @@
+import { Request, Response } from "express";
+
+import db from "../database/connection";
+import convertHourToMinutes from "../utils/convertHourToMinutes";
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: "Missing filters to search classes",
+      });
+    }
+  }
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule,
+    } = request.body;
+
+    const trx = await db.transaction();
+
+    try {
+      const insertedUsersIds = await trx("users").insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+      });
+
+      const user_id = insertedUsersIds[0];
+
+      const insertedClassesId = await trx("classes").insert({
+        subject,
+        cost,
+        user_id,
+      });
+
+      const class_id = insertedClassesId[0];
+
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to),
+        };
+      });
+
+      console.log(classSchedule);
+      await trx("class_schedule").insert(classSchedule);
+
+      await trx.commit();
+
+      return response.status(201).send();
+    } catch (error) {
+      await trx.rollback();
+
+      console.log(error);
+
+      return response.status(400).json({
+        error: "Unexpected error while creating a new class",
+      });
+    }
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 
+import routes from "./routes";
+
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
-
-app.get("/users", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
