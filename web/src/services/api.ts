0db5b46c2eabde8304b6ae176dc02cf5a62f2c89
FelipeDecome <felipe.decome@hotmail.com>
2020-08-09 12:36:45 -0300

    Api URL updated for production

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://proffynlwserver.herokuapp.com"
+    : "http://localhost:3333";
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: URL,
 });
 
 export default api;
