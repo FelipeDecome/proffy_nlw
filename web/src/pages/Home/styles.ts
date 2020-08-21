64a0fc3e8f90c704b9b612237b588566511e727b
Felipe Decome <felipe.decome@hotmail.com>
2020-08-21 12:20:11 -0300

    mobile problems solved

--- web/src/pages/Home/styles.ts
+++ web/src/pages/Home/styles.ts
@@ -3,11 +3,8 @@ import styled from "styled-components";
 export const LandingWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 
-  width: 100vw;
-  height: 100vh;
+  background: ${({ theme }) => theme.colors.primary};
 `;
 
 export const LandingMain = styled.main`
@@ -18,10 +15,9 @@ export const LandingMain = styled.main`
 
   height: 100%;
   width: 100%;
-  padding-bottom: 3.6rem;
+  padding: 3.6rem 0;
 
   color: ${({ theme }) => theme.colors.textInPrimary};
-  background: ${({ theme }) => theme.colors.primary};
 `;
 
 export const LandingLogoContainer = styled.div`
@@ -51,14 +47,6 @@ export const LandingImg = styled.img`
   width: 100%;
 `;
 
-export const LandingFooter = styled.footer`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  padding: 3.6rem 4rem;
-`;
-
 export const FooterButtonsContainer = styled.div`
   display: flex;
   align-items: center;
@@ -73,6 +61,7 @@ export const Greetings = styled.p`
   font-family: Poppins;
   font-size: 2rem;
   font-weight: 400;
+  max-width: 24rem;
 
   & span {
     font-weight: 700;
@@ -85,5 +74,48 @@ export const TotalConnections = styled.span`
 
   & > img {
     margin-left: 0.8rem;
+  }
+
+  @media (min-width: 700px) {
+    font-size: 1.4rem;
+  }
+`;
+
+export const LandingFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  padding: 3.6rem 4rem;
+
+  background: ${({ theme }) => theme.colors.background};
+
+  @media (min-width: 1100px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-areas: "
+      greetings totalconnections buttons buttons
+    ";
+    gap: 1.6rem;
+
+    ${FooterButtonsContainer} {
+      grid-area: buttons;
+    }
+
+    ${Greetings} {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      max-width: inherit;
+      grid-area: greetings;
+    }
+
+    ${TotalConnections} {
+      display: flex;
+      align-items: center;
+
+      grid-area: totalconnections;
+    }
   }
 `;
